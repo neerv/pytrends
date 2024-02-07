f9ca64952e6bf9633f3ada5d942dabdd4a7a81ab
Neer Varshney <varshney.neer@gmail.com>
2024-02-07 17:08:20 +0700

    Update request.py to include post instead of get

--- pytrends/request.py
+++ pytrends/request.py
@@ -86,7 +86,7 @@ class TrendReq(object):
                 else:
                     proxy = ''
                 try:
-                    return dict(filter(lambda i: i[0] == 'NID', requests.get(
+                    return dict(filter(lambda i: i[0] == 'NID', requests.post(
                         f'{BASE_TRENDS_URL}/explore/?geo={self.hl[-2:]}',
                         timeout=self.timeout,
                         proxies=proxy,
